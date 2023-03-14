841aa04c777f1a166642fd68620937464718902c
Anastasiya <anastashen8@gmail.com>
2023-03-14 21:19:30 +0300

    add Homework 6_add

--- Homework_6/Task2_6.py
+++ Homework_6/Task2_6.py
@@ -10,7 +10,9 @@
 
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4,-2,10,2,0,-9,8,10,-9, 0, -5, -5, 7]
 list_2 = []
+min_number = int(input())
+max_number = int(input())
 for i in range(len(list_1)):
-    if 5 < list_1[i] < 15:
+    if min_number < list_1[i] < max_number:
         list_2.append(i)
-print(list_2)
+print(*list_2)
